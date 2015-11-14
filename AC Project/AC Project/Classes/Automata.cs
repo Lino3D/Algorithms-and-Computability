e909ec8d43687389c53a73c6fd268bff98771253
Lino3D <kamiskijakub@gmail.com>
2015-11-14 10:02:12 +0100

    First Commit

---
+++ AC Project/AC Project/Classes/Automata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC_Project.Classes
+{
+    class Automata
+    {
+        int States;
+        List<char> Alphabet;
+        List<TransitionTable> TransitionTables;
+
+    }
+}
---
+++ AC Project/AC Project/Classes/TransitionTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC_Project.Classes
+{
+    class TransitionTable
+    {
+        int size
+        {
+            get;
+            set;
+        }
+        double[,] Table;
+       void createTable(int size)
+        {
+            Table = new double[size, size];
+        }
+    }
+}
