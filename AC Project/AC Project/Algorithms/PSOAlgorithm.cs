4395b0c0dd3da9c60731d0c02df26c00bcdc6452
michcio91 <michcio91@gmail.com>
2015-11-14 22:04:42 +0100

    Merging

--- AC Project/AC Project/Algorithms/PSOAlgorithm.cs
+++ AC Project/AC Project/Algorithms/PSOAlgorithm.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AC_Project.Classes;
 
 namespace AC_Project.Algorithms
 {
     class PSOAlgorithm
     {
+        List<Automata> automatas = new List<Automata>();
+        int n;
+        /*
+         * Where Automata ideal - searched problem
+         * alphabet - alphabet of the automaton
+         * n - number of particles
+         * */
+        public void ComputePSO( Automata ideal, int[] alphabet, int _n )
+        {
+            /* 1. Generate Particles, Velocity
+             * 2. Compute Error and Choose Local Bests
+             * 3. Choose Global Best
+             * 
+             * 
+             */
+            n = _n;
+            for( int i = 0 ; i < n; i++)
+            {
+                //Generate 2-states random Automatons
+                automatas.Add(Automata.GenerateParticle(2, alphabet));
+            }
+            int iterations = 0;
+            Words words = WordGenerator.GenerateWords(alphabet, alphabet.Count());
+            while( iterations < 500 )// && Error > MinError)
+            {
+
+
+                iterations++;
+            }
+
+        }
+
+    /*    int ChooseLocalBest()
+        {
+            int best = 0;
+            float ErrorMin = float.MaxValue;
+            for( int i = 0; i < n ; i++)
+            {
+               
+
+            }
+            return best;
+        }*/
     }
 }
