dcadfaab4f583a7dab2e1629c66fb5d44f7c2ff3
michcio91 <michcio91@gmail.com>
2015-11-14 14:30:37 +0100

    Merging

---
+++ AC Project/AC Project/Classes/Words.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC_Project.Classes
+{
+    class Words
+    {
+        int[][] Words;
+        int NumWords;
+        int NumAccepted;
+        int NumRejected;
+        public Words(int[][] words, int numwords)
+        {
+            NumWords = numwords;
+            for( int i = 0; i < NumWords; i++)
+                Array.Copy(words[i], Words[i], words[i].Count());
+        }
+
+        public int GetWordsNum()
+        {
+            return NumWords;
+        }
+
+        public int[] GetWord(int i)
+        {
+            return Words[i];
+        }
+    }
+}
