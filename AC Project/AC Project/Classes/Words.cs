195d8c3eb7e6a8c6edda6eadfe55bafd339706a8
michcio91 <michcio91@gmail.com>
2015-11-16 17:25:47 +0100

    minor changes

--- AC Project/AC Project/Classes/Words.cs
+++ AC Project/AC Project/Classes/Words.cs
@@ -9,12 +9,21 @@ namespace AC_Project.Classes
     public class Words
     {
         public int[][] _Words;
-        int NumWords;
+        int NumWords = 100;
         int NumAccepted;
         int NumRejected;
         public Words(int[][] words, int numwords)
         {
-            NumWords = numwords;
+            int length = 10;
+            _Words = new int[100][];
+            for (int i = 0; i < 100; i++ )
+            {
+                if (i != 0 && i % 10 == 0)
+                    length += 10;
+                _Words[i] = new int[length];
+
+            }
+                NumWords = numwords;
             for( int i = 0; i < NumWords; i++)
                 Array.Copy(words[i], _Words[i], words[i].Count());
         }
